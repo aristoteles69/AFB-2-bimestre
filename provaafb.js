6cb26d500098c663f574553445b4abc505237918
aristoteles <78802176+aristoteles69@users.noreply.github.com>
2021-06-17 15:13:17 -0300

    Update provaafb.js

--- provaafb.js
+++ provaafb.js
@@ -5,3 +5,24 @@ function someFun(params, callback) {
 
   return 1
 }
+// Segundo Quesito
+
+function someFn(number) {
+  const returnFunction = (value) => {
+    switch (value) {
+      case 3:
+        return 4
+      case 1:
+        return 5
+      case 5:
+        return 10
+    }
+  }
+
+  return returnFunction
+}
+// Terceiro Quesito
+
+function calculator(n1, n2) {
+  return callback => callback(n1, n2)
+}
